53486bc2916ded15e5c8d34478053ecf873734d7
IIIAnthonyIII <aadvl382016@outlook.com>
2023-12-28 14:39:55 -0500

    Tailwild install

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App(){
   return (
-    <div>App</div>
-    
+    <div className="h-screen flex justify-center items-center">
+      <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300">
+        Change theme
+      </button>
+    </div>
   )
 }
 
